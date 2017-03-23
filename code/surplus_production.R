715a89e5d7ebed95a723c31337f847617655b494
Katie Palof <katie.palof@alaska.gov>
2017-03-23 12:19:05 -0800

    initial surplus production code

---
+++ code/surplus_production.R
@@ -0,0 +1,6 @@
+#K.Palof
+# 3-13-17
+# Attempt at surplus production models in R using TropFishR package and prod_mod (data, plot = FALSE)
+# https://www.rdocumentation.org/packages/TropFishR/versions/1.0.0/topics/prod_mod
+
+# need data set with year, catch (lbs), pot lifts, CPUE
