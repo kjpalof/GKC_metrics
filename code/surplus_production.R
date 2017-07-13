9f59cc7ffe44961dfa3c9eae27210f0f68333188
Katie Palof <katie.palof@alaska.gov>
2017-07-13 14:19:47 -0800

    catch by season from fish tickets

--- code/surplus_production.R
+++ code/surplus_production.R
@@ -10,7 +10,7 @@
 ## Load packages ---------------------------------------------------
 library(tidyverse)
 library(reshape2)
-library(TropFishR)
+#library(TropFishR)
 
 ### load data ---------
 gkc <- read.csv("./data/gkc_fishticket_all_17.csv")
@@ -21,9 +21,14 @@ gkc %>% select(Season = SEASON, Area = I_FISHERY, numbers = NUMBERS, pounds = PO
 
 head(gkc1)
 
-### catch by season with effort -----------
+### fish ticket catch by season -----------
 gkc1 %>% group_by(Area, Season) %>% 
   summarise(number = sum(numbers), biomass = sum(pounds), pot_effort = sum(pots),
-              permits = length(unique(Cfec_no)))
-  
+              permits = length(unique(Cfec_no))) -> gkc2
+
+as.data.frame(gkc2 %>% filter(Area == "East Central GKC")) # checks out with what I have in excel surplus production file from 2013
+
+### Area models ----------------
+# each area has it's own model - one with fish ticket data and # permits
+#           - one with logbook data and # pot lifts.
 
