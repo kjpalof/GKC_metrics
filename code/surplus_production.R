ba5a914c35f4646f4caaf64f5ce0ce22a3b21638
Katie Palof <katie.palof@alaska.gov>
2017-07-13 16:23:20 -0800

    add in logbook data.  Need to add variable to combine these two.  Add season to logbook - seems easier, also shorter time series

--- code/surplus_production.R
+++ code/surplus_production.R
@@ -14,12 +14,20 @@ library(reshape2)
 
 ### load data ---------
 gkc <- read.csv("./data/gkc_fishticket_all_17.csv")
+gkc_log <- read.csv("./data/gkc_logbook_all_030117.csv")
 head(gkc)
+head(gkc_log)
 
+### select variables -------------
 gkc %>% select(Season = SEASON, Area = I_FISHERY, numbers = NUMBERS, pounds = POUNDS, 
                pots = POTS, Cfec_no = CFEC_NO) -> gkc1
-
 head(gkc1)
+
+# need to add season here to match fish tickets
+gkc_log %>% select(Year = YEAR, Area = I_FISHERY, no_crab = TARGET_SPECIES_RETAINED, 
+                   pots = NUMBER_POTS_LIFTED, Cfec_no = CFEC_NO) -> gkc1_log
+gkc1_log %>% mutate(Season)
+head(gkc1_log)
 
 ### fish ticket catch by season -----------
 gkc1 %>% group_by(Area, Season) %>% 
