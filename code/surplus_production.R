3cab9ebf89547f8e6f93847a7d5bc72d7c9ca9f2
Katie Palof <katie.palof@alaska.gov>
2017-08-23 09:22:26 -0800

    changes so that data is summarized by year instead of season.  Season in here is not correct or current.

--- code/surplus_production.R
+++ code/surplus_production.R
@@ -30,46 +30,59 @@ head(gkc)
 head(gkc_log)
 
 ### select variables -------------
-gkc %>% select(Season = SEASON, Area = I_FISHERY, numbers = NUMBERS, pounds = POUNDS, 
+# fish ticket data by year - this is more consistent since seasons
+#     are NOT correct in recent (since 2000) years
+gkc %>% select(Year = YEAR, Area = I_FISHERY, numbers = NUMBERS, pounds = POUNDS, 
                pots = POTS, Cfec_no = CFEC_NO) -> gkc1
+# by season - DON'T USE - use year instead
+#gkc %>% select(Season = SEASON, Area = I_FISHERY, numbers = NUMBERS, pounds = POUNDS, 
+#               pots = POTS, Cfec_no = CFEC_NO) -> gkc1
 head(gkc1)
 
 # need to add season here to match fish tickets
 gkc_log %>% select(Year = YEAR, Area = I_FISHERY, no_crab = TARGET_SPECIES_RETAINED, 
                    pots = NUMBER_POTS_LIFTED, Cfec_no = CFEC_NO) -> gkc1_log
-gkc1_log %>% mutate(Season = ifelse (Year == 2000, "Oct1999 - Sep00", 
-                              ifelse(Year == 2001,"Oct2000 - Sep01", 
-                               ifelse(Year == 2002, "Oct2001 - Sep02", 
-                                ifelse(Year == 2003, "Oct2002 - Sep03", 
-                                 ifelse(Year == 2004, "Oct2003 - Sep04", 
-                                  ifelse(Year == 2005, "Oct2004 - Sep05", 
-                                    ifelse(Year == 2006, "Oct2005 - Sep06", 
-                                      ifelse(Year == 2007, "Oct2006 - Sep07", 
-                                       ifelse(Year == 2008, "Oct2007 - Sep08", 
-                                        ifelse(Year == 2009, "Oct2008 - Sep09", 
-                                         ifelse (Year == 2010, "Oct2009 - Sep10", 
-                                          ifelse(Year == 2011, "Oct2010 - Sep11", 
-                                           ifelse(Year == 2012, "Oct2011 - Sep12", 
-                                            ifelse(Year == 2013, "Oct2012 - Sep13", 
-                                             ifelse(Year == 2014, "Oct2013 - Sep14", 
-                                              ifelse(Year == 2015, "Oct2014 - Sep15", 
-                                               ifelse(Year == 2016, "Oct2015 - Sep16", 
-       ifelse(Year == 2017, "Oct2016 - Sep17", ""))))))))))))))))))) -> gkc1_log
+#gkc1_log %>% mutate(Season = ifelse (Year == 2000, "Oct1999 - Sep00", 
+#                              ifelse(Year == 2001,"Oct2000 - Sep01", 
+#                               ifelse(Year == 2002, "Oct2001 - Sep02", 
+#                                ifelse(Year == 2003, "Oct2002 - Sep03", 
+#                                 ifelse(Year == 2004, "Oct2003 - Sep04", 
+#                                  ifelse(Year == 2005, "Oct2004 - Sep05", 
+#                                    ifelse(Year == 2006, "Oct2005 - Sep06", 
+#                                      ifelse(Year == 2007, "Oct2006 - Sep07", 
+#                                       ifelse(Year == 2008, "Oct2007 - Sep08", 
+#                                        ifelse(Year == 2009, "Oct2008 - Sep09", 
+#                                         ifelse (Year == 2010, "Oct2009 - Sep10", 
+#                                          ifelse(Year == 2011, "Oct2010 - Sep11", 
+#                                           ifelse(Year == 2012, "Oct2011 - Sep12", 
+#                                            ifelse(Year == 2013, "Oct2012 - Sep13", 
+#                                             ifelse(Year == 2014, "Oct2013 - Sep14", 
+#                                              ifelse(Year == 2015, "Oct2014 - Sep15", 
+#                                               ifelse(Year == 2016, "Oct2015 - Sep16", 
+#       ifelse(Year == 2017, "Oct2016 - Sep17", ""))))))))))))))))))) -> gkc1_log
 head(gkc1_log)
-### logbook catch by season
-gkc1_log %>% group_by(Area, Season, Year) %>% 
-  summarise(no_crabs = sum(no_crab, na.rm = TRUE), pot_effort = sum(pots, na.rm = TRUE),
+### logbook catch by year -----
+gkc1_log %>% group_by(Area, Year) %>% 
+  summarise(no_crabs = sum(no_crab, na.rm = TRUE), 
+          pot_effort = sum(pots, na.rm = TRUE),
             permits = length(unique(Cfec_no))) -> gkc2_log
+# this was done by season.  Don't use. 
+#gkc1_log %>% group_by(Area, Season, Year) %>% 
+#  summarise(no_crabs = sum(no_crab, na.rm = TRUE), pot_effort = sum(pots, na.rm = TRUE),
+#            permits = length(unique(Cfec_no))) -> gkc2_log
 
 
-### fish ticket catch by season -----------
-gkc1 %>% group_by(Area, Season) %>% 
+### fish ticket catch by year -----------
+gkc1 %>% group_by(Area, Year) %>% 
   summarise(number = sum(numbers, na.rm = TRUE), biomass = sum(pounds, na.rm = TRUE), ft_pots = sum(pots, na.rm=TRUE),
               ft_permits = length(unique(Cfec_no))) -> gkc2
-gkc2 %>% left_join(season_year) -> gkc2
+
+#gkc1 %>% group_by(Area, Season) %>% 
+#  summarise(number = sum(numbers, na.rm = TRUE), biomass = sum(pounds, na.rm = TRUE), ft_pots = sum(pots, na.rm=TRUE),
+#            ft_permits = length(unique(Cfec_no))) -> gkc2
+#gkc2 %>% left_join(season_year) -> gkc2
 
 as.data.frame(gkc2 %>% filter(Area == "East Central GKC")) # checks out with what I have in excel surplus production file from 2013
-gkc2 %>% filter(Season == "Oct2006 - Sep07")
 
 ### Area models ----------------
 # each area has it's own model - one with fish ticket data and # permits
@@ -121,7 +134,7 @@ write.csv(gkc3_east, './results/east_central_gkc.csv', row.names = FALSE)
 ### prod model equlibrium ----------
 # data is dataframe with year (year vector), Y (catch in weight), f (fishing effort),
 gkc3_east %>% select(year = Year, Y = biomass, f = pots) %>% filter(year <2017) %>% 
-  filter(year >= 1984)->east_input
+  filter(year >= 1985)->east_input
 east_input %>% as.data.frame(east_input) %>% select(-Area)-> east_input1
 
 equil_ec <- prod_mod(east_input1, plot = TRUE)
