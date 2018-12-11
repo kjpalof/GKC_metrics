e92b3a3b64653b52c53121252295954d3df0c8d3
Katie Palof <katie.palof@alaska.gov>
2018-12-10 15:20:11 -0900

    attempt to look at some tier 5 calculations similar to federally managed stocks.  average in harvest over 2 time frames

--- code/tier5.R
+++ code/tier5.R
@@ -23,3 +23,35 @@ gkc %>% select(Year = YEAR, Area = I_FISHERY, numbers = NUMBERS, pounds = POUNDS
 #gkc %>% select(Season = SEASON, Area = I_FISHERY, numbers = NUMBERS, pounds = POUNDS, 
 #               pots = POTS, Cfec_no = CFEC_NO) -> gkc1
 head(gkc1)
+
+### summarise catch by year ---------
+mngmt_areas <- c("East Central GKC", "Icy Strait GKC", "Lower Chatham Strait GKC", "Mid-Chatham Strait GKC", 
+                 "North Stephens Passage GKC", "Northern GKC")
+
+gkc1 %>% 
+  group_by(Area, Year) %>% 
+  summarise(harvest = sum(pounds), no.crab = sum(numbers)) %>% 
+  filter(Area %in% mngmt_areas) -> gkc1_year
+
+# averages ----
+gkc1_year %>% 
+  group_by(Area) %>% 
+  summarise(all.avg = mean(harvest, na.rm = TRUE)) -> all_yrs
+gkc1_year %>% 
+  filter(Year > 1989 & Year < 2001) %>% 
+  group_by(Area) %>% 
+  summarise(y90s.avg = mean(harvest, na.rm = TRUE)) -> y90s
+
+all_yrs %>% 
+  left_join(y90s) -> avgs
+
+gkc1_year %>% 
+  left_join(avgs) -> gkc1_year_avgs
+
+# plots -----
+gkc1_year_avgs %>% 
+  ggplot(aes(x = Year, y = harvest)) +
+  geom_point() +
+  facet_wrap(~ Area) +
+  geom_hline(aes(yintercept = all.avg), color = "grey25") #+
+ 
